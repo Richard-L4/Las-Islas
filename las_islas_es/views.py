fbce63306b049ec4cccade645d85124172c1ba50
Richard-L4 <rwells945@gmail.com>
2026-04-28 20:58:57 +0100

    add html files and add start of nav bar

--- las_islas_es/views.py
+++ las_islas_es/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    return render(request, 'register.html', {'active_tab': 'register'})
